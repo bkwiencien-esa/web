ccc95746899c6b959e3b59dfd215d811a9a3a308
Bob Kwiencien <bkwiencien@gmail.com>
2020-11-27 19:03:55 -0500

    house keeping still

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #! /Users/bobkwiencien/anaconda/bin/python
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Lizard Lips!!"
+    return render_template('base.html')
 
 if __name__=='__main__':
-    app.run(debuf=True)    
+    app.run(debug=True)    
 
 
