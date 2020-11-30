b16fe03c4d6327e18ddc8de51f78336bbf3e2783
Bob Kwiencien <bkwiencien@gmail.com>
2020-11-29 23:44:37 -0500

    jinga2 work

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #! /Users/bobkwiencien/anaconda/bin/python
 from flask import Flask, render_template
+import psycopg2 as pg
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+	msg="What would you like to do?"
+	return render_template('index.html',messo=msg)
 
 if __name__=='__main__':
     app.run(debug=True)    
