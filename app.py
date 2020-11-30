f7e3648360f58957c9d257e0cb7d9a00c559f3d3
Bob Kwiencien <bkwiencien@gmail.com>
2020-11-30 09:26:08 -0500

    solved formatting issue

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 #! /Users/bobkwiencien/anaconda/bin/python
 from flask import Flask, render_template
 import psycopg2 as pg
-
+conn = ""
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	msg="What would you like to do?"
+	msg = buildDropDown()
 	return render_template('index.html',messo=msg)
 def buildDropDown():
-	return("nothing yet")
+	return("<center><h4>What table's data would you like to examine?</h1></center>")
 if __name__=='__main__':
     app.run(debug=True)    
 
