486988cb028fa5935ca0ba72fbbee9d82df928c9
Bob Kwiencien <bkwiencien@gmail.com>
2020-11-30 21:19:31 -0500

    add form

--- app.py
+++ app.py
@@ -5,14 +5,26 @@ conn = ""
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/dropo")
 def index():
 	conn = pg.connect(
     host="localhost",
     database="fw_telemetry")
 	msg = buildDropDown(conn)
-	return render_template('index.html',messo=msg)
+	return render_template('index.html',messo="which table would you like to examine?",frm=msg)
+	def dropo():
+		return render_template('index.html',mess="done")
 def buildDropDown(c):
-	return("xoxoxo")
+	command = '<center><form action="/dropo"> '
+	command = command + '<label for="tabs">Choose a table:</label> '
+	command = command + '<select name="tabs" id="tabs"> '
+	command = command + '<option value="fw_asset">fw_asset</option> '
+	command = command + '<option value="fw_channel_state">fw_channel_state</option> '
+	command = command + '</select> '
+	command = command + '<br><br> '
+	command = command + '<input type="submit" value="Submit"> '
+	command = command + '</form></center>'
+	return(command)
 if __name__=='__main__':
     app.run(debug=True)    
 
