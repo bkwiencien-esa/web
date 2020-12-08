298c1e723f6f90eb18754f61afabba95ed8f570f
Bob Kwiencien <bkwiencien@gmail.com>
2020-12-07 23:11:26 -0500

    beginning of html table

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ def index():
 	return render_template('index.html',messo="<center><h4>What table's data would you like to examine?</h1></center>",frm=msg,frmv=view)	
 @app.route("/dropo")	
 def dropo():
-	conn = getConnection()
+	conn = getConnection()  
 	tabname=request.args.get('tabs')
 	limit=request.args.get('limito')
 	results = getTableHTML(conn,tabname)
-	return render_template('tabledata.html',tdata='done')
+	return render_template('tabledata.html',tablename=tabname,tdata=results)
 def buildDropDown(c):
 	c1 = c.cursor()
 	c1.execute("select tablename from pg_tables where tablename like 'fw_%' order by tablename")
@@ -29,7 +29,7 @@ def buildDropDown(c):
 		command = command + '<option value='+tabo[0]+'>'+tabo[0]+'</option>\n'
 	command = command + '</select> '
 	command = command + '<br><br> '
-	command = command + '<label for="limito">Choose limit:</label>'
+	command = command + '<label  for="limito">Choose limit:</label>'
 	command = command + '<select name="limito" id="limit">'
 	command = command + '<option value="10">10</option>'
 	command = command + '<option value="100">100</option>'
@@ -42,8 +42,12 @@ def buildDropDown(c):
 def getTableHTML(c,tname):
 	ll = getColumns(c,tname)
 	selectstmt=buildSelect(tname,ll)
-	tabhtml='<table id="tab1">'
-	return("xoxox")	
+	tabhtml='<table id="tab1"> '
+	tabhtml=tabhtml+'<tr> '
+	for cn in ll:
+		tabhtml=tabhtml+'<th>'+cn[0]+'</th> '
+	tabhtml=tabhtml+'</tr></table>' 	
+	return(tabhtml)	
 def buildViewView(conn):
 	return("<center><b>not done yet<b></center>")	
 def getColumns(c,l):
@@ -51,11 +55,15 @@ def getColumns(c,l):
 	c1.execute("select column_name from information_schema.columns where table_name='{}' order by ordinal_position".format(l))
 	colrec= c1.fetchall()
 	listo =[xx for xx in colrec]
-	for j in listo:
-		print(j[0])
 	return(listo)
 def buildSelect(t,l):
-	return('not done')	
+	selectlist=""
+	for c in l:
+		selectlist = selectlist + c[0]+", "
+	selectlist = selectlist[:-2]	
+	query = "select " + selectlist + " from " + t
+	print(query)
+	return(query)	
 def getConnection():
 	conn = pg.connect(
 	host="localhost",
